e555382b49ba1341c6aca2800e378d20e8ad4157
Jason Washburn <jason.washburn@gmail.com>
2023-07-21 20:23:15 -0500

    feat: :tada: initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/jasonwashburn/go-bookings/pkg/config"
+	"github.com/jasonwashburn/go-bookings/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
